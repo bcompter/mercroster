7a8e1c7ae291bc5e75e109fd11f06236734e53fd
bombaijin <bombaijin@baf032e7-b913-4013-9d85-81630d07942e>
2010-08-06 19:20:08 +0000

    + Fix Parser.php issue

---
+++ mercroster/includes/Parser.php
@@ -0,0 +1,35 @@
+<?php
+class Parser
+{
+  /**
+   * Funtion used to strip all kind of nasty thing out of _POST data
+   * @param <string> $data
+   * @return <string>
+   */
+  protected function strip($data)
+  {
+    require("htdocs/dbsetup.php");
+    $data=stripslashes($data);
+    $data=mysql_real_escape_string($data);
+    $data=strip_tags($data);
+    return $data;
+  }
+  
+  /**
+   * Function used to check whatever given argument in positive integer
+   * @param $data
+   * @return boolean
+   */
+  protected function checkposint($data)
+  {
+    if(ctype_digit($data) && $data>0)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+}
+?>
