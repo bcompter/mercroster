b053872f5feb2c387db4beb55127d941e03d059e
bombaijin <bombaijin@baf032e7-b913-4013-9d85-81630d07942e>
2010-08-06 17:52:09 +0000

    some fixes to the databases

--- mercroster/update_roster.sql
+++ mercroster/update_roster.sql
@@ -1,11 +1,5 @@
----
----Add call sign column to crew table
----
 ALTER TABLE `crew` ADD COLUMN `callsign` varchar(30);
 
----
----Add troid column to technicalreadouts
----
 ALTER TABLE `equipment` ADD COLUMN `troid` int(10);
 
 --
