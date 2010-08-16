6021d6d14da437eed4fe93136e612bb3ea97a942
bombaijin <bombaijin@baf032e7-b913-4013-9d85-81630d07942e>
2010-08-16 19:27:07 +0000

    screwed up htdocs

--- mercroster/htdocs/dbsetup.php
+++ mercroster/htdocs/dbsetup.php
@@ -1,12 +1,12 @@
 <?
 $dbhost = 'localhost:3306';
-$dbuser = 'root';
-$dbpass = '';
+$dbuser = 'INSERT_HOST';
+$dbpass = 'INSERT_PASSWORD';
 
 $conn = mysql_connect($dbhost, $dbuser, $dbpass);
 if(! $conn )
 {
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db( 'flaming_devil_monkeys' );
+mysql_select_db( 'mercroster' );
 ?>
